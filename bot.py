9e9b1da111f8ceef4a2948723f65760e0f3b3aa4
rakuzanzxc <weds123saa@gmail.com>
2026-03-05 12:00:43 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,18 @@
 import telebot
 import os
 
-TOKEN = "8740639535:AAHbyI0EszZcmguJSFPFBJCZ0ZrKvb4J1MQ"
+# Получаем токен из переменных окружения сервера
+TOKEN = os.getenv('BOT_TOKEN')
 
-if not TOKEN:
-    raise Exception("TOKEN не найден! Добавь его в Railway Variables")
+# Проверка: если токен не найден, бот выдаст понятную ошибку вместо вылета
+if TOKEN is None:
+    raise ValueError("Ошибка: Переменная BOT_TOKEN не установлена в настройках Railway!")
 
 bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Привет! Я работаю 24/7 на Railway!")
+
+# Важно для Railway: добавьте бесконечный цикл
+bot.infinity_polling()
