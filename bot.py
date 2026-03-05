fc2e03dbde73eed64eb1fbf1eacd90986359931a
rakuzanzxc <weds123saa@gmail.com>
2026-03-05 11:55:21 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import telebot
 import os
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = "8740639535:AAHbyI0EszZcmguJSFPFBJCZ0ZrKvb4J1MQ"
 
 if not TOKEN:
     raise Exception("TOKEN не найден! Добавь его в Railway Variables")
