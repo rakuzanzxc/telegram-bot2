dbf1138c6c362737f01ec842020e4d3a38089ff0
rakuzanzxc <weds123saa@gmail.com>
2026-03-05 20:11:08 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import os
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
 from aiogram.types import FSInputFile, InlineKeyboardMarkup, InlineKeyboardButton
